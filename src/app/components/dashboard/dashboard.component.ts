2e1b21545e8f9bc7f683c4492bbf6723c4c6a110
Matheus Fantinel <matfantinel@gmail.com>
2018-12-17 22:53:52 -0200

    Load Collection Chart Data

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BooksService } from 'src/app/shared/services/books/books.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
   collectionChartOptions: any;
 
-  constructor() { }
+  constructor(public booksService: BooksService) {}
 
   ngOnInit() {
     this.initCollectionChartOptions();
@@ -17,36 +17,57 @@ export class DashboardComponent implements OnInit {
 
   public initCollectionChartOptions() {
     this.collectionChartOptions = {
-      tooltip : {
-          trigger: 'item',
-          formatter: "{a} <br/>{b} : {c} ({d}%)"
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
       },
-      series : [
+      series: [
         {
-              name: 'Books',
-              type: 'pie',
-              radius : '55%',
-              center: ['40%', '50%'],
-              data:[
-                {value:50, name:'Not Read', itemStyle: { color: '#4DA167' }},
-                {value:3, name:'Started', itemStyle: { color: '#5E565A' }},
-                {value:67, name:'Finished', itemStyle: { color: '#E15554' }},
-              ],
-              itemStyle: {
-                emphasis: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                },
-              },
-              animationType: 'scale',
-              animationEasing: 'elasticOut',
-              animationDelay: function (idx) {
-                  return Math.random() * 200;
-              }
+          name: 'Books',
+          type: 'pie',
+          radius: '55%',
+          center: ['40%', '50%'],
+          data: this.loadCollectionChartDate(),
+          itemStyle: {
+            emphasis: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          },
+          animationType: 'scale',
+          animationEasing: 'elasticOut',
+          animationDelay: function(idx) {
+            return Math.random() * 200;
           }
+        }
       ]
     };
   }
 
+  loadCollectionChartDate() {
+    let books = this.booksService.getBooks();
+    if (!books) {
+      return null;
+    }
+
+    let result = [];
+
+    let unreadBooks = books.filter(q => !q.read && !q.startedDate).length;
+    if (unreadBooks > 0) {
+      result.push({ value: unreadBooks, name: 'Not Read', itemStyle: { color: '#5E565A' } });
+    }
+
+    let startedBooks = books.filter(q => !q.read && q.startedDate).length;
+    if (startedBooks > 0) {
+      result.push({ value: startedBooks, name: 'Started', itemStyle: { color: '#FF934F' } });
+    }
+
+    let readBooks = books.filter(q => q.read).length;
+    if (readBooks > 0) {
+      result.push({ value: readBooks, name: 'Finished', itemStyle: { color: '#4DA167' } });
+    }
+
+    return result;
+  }
 }
